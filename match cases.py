538e607fdcd805b1f017b99cd429e5df23e4b650
Anshu Sarkar <anshusarkar11@gmail.com>
2022-11-02 19:06:30 +0530

    Echo

---
+++ match cases.py
@@ -0,0 +1,4 @@
+choice = input("Enter the a choice : ")
+match choice : #match cases are like Switch cases in python and is available in python 3.11 and later 
+    case "One":
+        print("Hello, python 3.11 !")
