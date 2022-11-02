f9e54199527f6e1ff2f3098eb5941169d23e0827
Anshu Sarkar <anshusarkar11@gmail.com>
2022-11-02 20:07:25 +0530

    Echo

---
+++ Switch case.py
@@ -0,0 +1,10 @@
+def switch (inp):
+    if inp == "One":
+        return "Input was One"
+    elif inp == "Two":
+        return "Input was Two"
+    else :
+        return "Wrong input"
+inp=input("Enter a value :")
+
+print(switch(inp))
