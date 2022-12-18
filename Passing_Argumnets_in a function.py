0f0f62558f06c702adc97bd249c7102b3180c3e3
Anshu <anshusarkar11@gmail.com>
2022-12-18 18:15:07 +0530

    echo

--- Passing_Argumnets_in a function.py
+++ Passing_Argumnets_in a function.py
@@ -1,7 +1,13 @@
-def average (*numbers): # '*' is character yhat gets used to pass arbitary argumunets in functions in python 
-    sum = 0 
+def average (*numbers): # '*' is character that get's used to pass a tuple as an argument or a parameter 
+    print(type(numbers))
+    sum = 0
     for x in numbers :
         sum = sum + x
     print("The average is : " , sum/len(numbers))
     
 average(1,2,3)
+
+def name (**names): #'**' is character that get's used to pass a dictionary as an argumnet 
+    print(type(names))
+    print("Hello !",names["Firstpreson"],names["Secondperson"],names["Thirdperson"])
+name(Firstpreson = "Anshu" , Secondperson="X", Thirdperson="Y" )
---
+++ dictionary.py
@@ -0,0 +1,2 @@
+person = { "name" : "Anshu" , "title": "Sarkar" , "Birth_Date":19.01 }
+print(person["name"])
