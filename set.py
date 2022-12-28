1977b0b8200f50ec73672bf4da4adbedb7ddb7fd
Anshu <anshusarkar11@gmail.com>
2022-12-28 17:58:47 +0530

    echo

---
+++ set.py
@@ -0,0 +1,6 @@
+s=set() #sets must be decleared with the keyword 'set' along with '()' 
+print(type(s))
+s={1,"Zero",1.2,"Science"}
+print(s)
+for _ in s : #To print values in a set a varriable get's used to print & acess the values of a set 
+    print(_)
