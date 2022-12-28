6a4f9d7adacc7241d0a93afd6cc061b0664c6777
Anshu <anshusarkar11@gmail.com>
2022-12-28 18:00:05 +0530

    echo

--- set.py
+++ set.py
@@ -1,6 +1,6 @@
 s=set() #sets must be decleared with the keyword 'set' along with '()' 
 print(type(s))
-s={1,"Zero",1.2,"Science"}
+s={1,"Zero",1.2,"Science"}#Elements in a set are alaways stored in randome manner and get's printed in random manner
 print(s)
 for _ in s : #To print values in a set a varriable get's used to print & acess the values of a set 
     print(_)
