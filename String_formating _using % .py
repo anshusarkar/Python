aebabecaf70593eb4f8c4cb84a4428a5981b1f45
Anshu <anshusarkar11@gmail.com>
2023-01-14 17:15:44 +0530

    echo

---
+++ String_formating _using % .py
@@ -0,0 +1,3 @@
+name="Anshu"
+age=19
+print("Hello ! %s" % name , "You are %s years old" % age)
---
+++ f_String.py
@@ -0,0 +1,2 @@
+name="Anshu"                                     #f_strings are used to format a place in a long string with a variable 
+print(f"Hello! {name},This is called a f string ")
