db21f393acbb85e6ac444d72a04674c2448e2e85
Anshu <anshusarkar11@gmail.com>
2023-01-16 18:26:56 +0530

    echo

--- File_Handaling/File_Writing.py
+++ File_Handaling/File_Writing.py
@@ -1,3 +1,7 @@
 
 fil=open(f"c.txt",'w')
 fil.write("This context is added with write method")
+fil.close()
+#To close file automatacally use with key word 
+with open("c.txt", 'a') as fil:
+    fil.write("\nWith the help of the keyword with file closes automatically ")
