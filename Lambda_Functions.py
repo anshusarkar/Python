fd0c27d695411be41ad46bdb2f5303bdb7ca4167
Anshu <anshusarkar11@gmail.com>
2023-01-18 21:01:49 +0530

    echo

--- Lambda_Functions.py
+++ Lambda_Functions.py
@@ -1,6 +1,7 @@
 #Lambda functions are a type of functions creation in which functions get's created in one line 
-
+# syntax : function_name = lambda parameters : return values of the parameters
 inp=int(input("Enter a value : "))
 squre = lambda x : x*x
-
+avg = lambda x,y,z : (x+y+z)/3
 print(squre(inp))
+print(avg(3,2,4))
