75782020c179bcf93e2107554c412296a4b43628
Anshu <anshusarkar11@gmail.com>
2023-01-31 11:22:35 +0530

    echo

--- Functions/Inheritance.py
+++ Functions/Inheritance.py
@@ -1,10 +1,9 @@
 class C1:
     def method():
-        print("This is a method call of class c1 method")
-class C2(C1): #To inherite a method of a class the entire class get's passed as a parameter for that class
-    def method():
         print("This is a method call of class C1 through C2 class")
-a = C1
+class C2(C1): #To inherite a method of a class the entire class get's passed as a parameter for that class
+
+    pass
+
+a = C2
 a.method()
-a2 = C2
-a2.method()
