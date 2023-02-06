3dceb6acf44919921a04033b753adc3bf2f94d28
Anshu <anshusarkar11@gmail.com>
2023-02-06 20:47:15 +0530

    echo

--- __dict__.py
+++ __dict__.py
@@ -1,8 +1,10 @@
 class Hello:
-     def __init__(self, name, age):
-      self.name = name
-      self.age = age
-      pass
-
+      """__dict__ get's used to pack up a constrouctor's conatins into a list """
+      def __init__(self, name, age): 
+            self.name = name
+            self.age = age
+            pass
+      
 h=Hello("Anshu",30)    
+print( h.__doc__ )
 print(h.__dict__)
