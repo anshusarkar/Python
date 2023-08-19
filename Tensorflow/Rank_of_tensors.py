4a0dd3eaaa0448e969c1d5bb6e14b1bd97b57738
Anshu <anshusarkar11@gmail.com>
2023-08-19 15:47:52 +0530

    echo

---
+++ Tensorflow/Rank_of_tensors.py
@@ -0,0 +1,10 @@
+import tensorflow as tf 
+
+Rank_1_tensor = tf.Variable(["Test"], tf.string)
+
+# Rank of a tensor is nothing but it's dimension 
+
+Rank_2_tensor = tf.Variable(["Test","One"] , ["Ok" , "Echo"] , tf.string)
+
+
+print (Rank_2_tensor)
--- Tensorflow/Zero.py
+++ Tensorflow/Zero.py
@@ -1,3 +1,10 @@
 import tensorflow as tf
 tf.__version__
-print(tf.config.list_physical_devices('GPU'))
+#print(tf.config.list_physical_devices('GPU'))
+
+#To declear a Variable in tensorflow tf.Variable get's used
+
+string = tf.Variable("This is a string", tf.string)
+number = tf.Variable(325, tf.int64)
+floating = tf.Variable(9.32 , tf.float64)
+
