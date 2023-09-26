9e1b07f93b1cab7c271135db993d8f601172e1e4
Anshu <anshusarkar11@gmail.com>
2023-09-26 21:18:20 +0530

    echo

---
+++ Functions/return_annotation.py
@@ -0,0 +1,39 @@
+#First have to know What annotations are ? => It's more like a soft type declaretaion or in the other words even after declaring a 
+#type of input to a varriable diffrent type of value can be inserted into it where in type declaration it isn't supposed to
+
+var : str = "Hello"
+var = 123 # Even after the warnig the code will run as it's a soft type declation it happens because python is dynamicly typed or in 
+          # other words it set's type before compilation in PVM i.e python virtual machine through python interprater as python is both 
+          # interprated and compiled like java used to be 
+print(var)
+
+var2 : str = 141
+
+print(var2)
+
+var3 : 'power'  # Even 'power' is an annotation though can't be checked using __annotaion__
+
+# to check it's annotation 
+from inspect import get_annotations
+
+print(get_annotations(var2))
+
+
+def function()->"This is passed using ->": # this will set annotation for a return statement
+    
+    return 
+
+print(function.__annotations__ ) #This will return the string in the function line after -> in a dictionary
+
+# EX -
+
+def power(Voltage: 'in Volts'   , Current :'in Ampher' )-> 'in watts':
+    return  Voltage*Current 
+
+print(power(5,2))
+
+print(power.__annotations__)
+
+# Conclusion - F strings is much better.
+# and annotations can be used to declare a varriable that's value might change according to the input of the varriable making it more varriable 
+# or a dynamic varriable  
