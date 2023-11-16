ed33e7be39783e6ea078c6a0453a04c69d368530
Anshu <anshusarkar11@gmail.com>
2023-11-16 17:00:26 +0530

    echo

---
+++ Pytorch/Typehinting.py
@@ -0,0 +1,4 @@
+val = int # this is called type casting
+val2 : int # This is called type hinting 
+# As python is a dynamicly typed lang it type hinting makes hintsd the type of 
+# the var during runtime
