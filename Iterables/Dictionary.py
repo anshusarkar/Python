b6fbd257f791afbc5d6ba8d48d2fd53b8c920b04
Anshu <anshusarkar11@gmail.com>
2023-12-07 11:47:04 +0530

    echo

---
+++ Iterables/Dictionary.py
@@ -0,0 +1,17 @@
+Identity = {
+    "name" : "Anshu Sarkar" ,
+    "adress" : "Berhampure" ,
+    "email" : "anshu@anshu.com"
+}
+
+print(Identity["adress"])
+
+Student = {
+   "Anshu":  1 ,
+   "Ram" :2  ,
+   "Lelouch": 3  
+}
+
+for i in Student:
+    print(Student[i])
+    
