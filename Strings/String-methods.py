0efbbe196309e11905e0549f9e0b1f1529586695
Anshu Sarkar <anshusarkar11@gmail.com>
2024-02-21 19:44:12 +0530

    echo

--- Strings/String-methods.py
+++ Strings/String-methods.py
@@ -16,3 +16,22 @@ print(int("12345") + int("54321"))
 print("Mountains".upper())
 
 print("Montains".lower())
+
+# join function to concatinate all list elements
+
+# joing list elements with "space" in between
+
+l = ["This" , "is" , "a" ,"sentence"]
+
+print(" ".join(l))
+
+# joining with "..." in between
+
+print("...".join(l))
+
+# split function splits a sentence into a list
+
+S = "Hello, wolrd !"
+
+print(S.split())
+
