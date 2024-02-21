c943a4ff3db620f768095efb0d3716b3a747bcf1
Anshu Sarkar <81322177+anshusarkar@users.noreply.github.com>
2024-02-21 10:15:17 +0000

    echo

---
+++ Strings/strip.py
@@ -0,0 +1,15 @@
+# To strip spaces out of strings use .strip()
+print("Hello ".strip())
+
+print(" hello".lstrip())
+
+print("hello ".rstrip())
+
+print("The number of times e ocurs in this string ".count("e"))
+
+print("Forest".endswith("rest"))
+
+print("123456789".isnumeric())
+
+print(int("12345") + int("54321"))
+
