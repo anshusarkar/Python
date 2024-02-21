43b9cc64e8871e145e699c90e0efdcadac07c6b4
Anshu Sarkar <anshusarkar11@gmail.com>
2024-02-21 20:27:30 +0530

    echo

--- Coursera/celsius-to-ferhenite.py
+++ Coursera/celsius-to-ferhenite.py
@@ -1,8 +1,9 @@
-# Conerting every 10th degit in range 10-100 from celsius to ferhenite
+# Conerting every 10th degit in range 10-100 from ferhenite to celsius
 
 def fun(val)->int:
     return (val-32)*(5/9)
 
 for i in range(10,110,10):
-    fer = fun(i)
-    print(f"The Value of {i}° celsius is ferhenite is : " , fer , "° ferhenite" )
+    cel = fun(i)
+    #print(f"The Value of {i}° ferhenite in celsius is : " , fer , "° ferhenite" )
+    print("{:>3}° F | {:>6.2f}° C".format(i,cel))
--- Strings/String_Formating .py
+++ Strings/String_Formating .py
@@ -13,3 +13,16 @@ tax = 0.18
 print("The prince of the product is : {}".format(price))
 
 print("The absolute cost of the product with tax is : {:.2f}".format(price+price*tax))
+
+
+# Conerting every 10th degit in range 10-100 from ferhenite to celsius
+
+def fun(val)->int:
+    return (val-32)*(5/9)
+
+for i in range(10,110,10):
+    cel = fun(i)
+    print("{:>3}° F | {:>6.2f}° C".format(i,cel))
+    
+# The :>3 specifies to print values of varriables to be allinged at right by three number of spaces
+# and for :>6 specifies to print values following 6 spaces by right 
