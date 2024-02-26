36f9d92dbed1028c523e7af9dbd0f0b64497e34f
Anshu Sarkar <anshusarkar11@gmail.com>
2024-02-26 21:53:40 +0530

    echo

--- Fun-progs/Neo_hiragana_fonts.py
+++ Fun-progs/Neo_hiragana_fonts.py
@@ -1,6 +1,8 @@
 # Hey you made it by your own insperations ! 
+# Install the Neo theme press ctrl + k + t and select it 4344
 import random 
 import time 
+
 
 l = ['あ', 'い', 'う', 'え', 'お',
     'か', 'き', 'く', 'け', 'こ',
@@ -15,7 +17,7 @@ l = ['あ', 'い', 'う', 'え', 'お',
 
 
 
-while True :
+def print_fonts(l:list) :
     
     random_element = random.choice(l)
     
@@ -27,3 +29,6 @@ while True :
     
     print(f"\t{second_rand_element}\t" ,random_element,f"\t{third_rand_element}\t" ,end=" ") # end = " " is used to print all values horizontaly 
                                                                                              # python's print function prints values in a line like Sysout in java
+    print_fonts(l)
+
+print_fonts(l)
