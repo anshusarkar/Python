f20f987cbfb57dd002266277fbe06aaa3aee1f07
Anshu Sarkar <anshusarkar11@gmail.com>
2024-03-03 14:57:17 +0530

    echp

--- Dealing_with_CSV/Writing_CSV_( With_dicts ).py
+++ Dealing_with_CSV/Writing_CSV_( With_dicts ).py
@@ -4,7 +4,7 @@ print(os.getcwd())
 
 os.chdir(os.path.join(os.getcwd(),"Dealing_with_CSV"))
 
-print(os.getcwd())
+print(os.getcwd()) # That code is written by you obously as the temp code runner can't cd into right direcory from terminal 
 
 users = [
     {"name" : "Human", "username" :"Hue", "Department": "IT analyst"},
@@ -19,7 +19,7 @@ with open('Department.csv', 'w') as D_CSV :
     writer.writeheader()
     writer.writerows(users)
     
-if  os.path.exists(os.path.join(os.getcwd(), "Department.csv")) == True :
+if  os.path.exists(os.path.join(os.getcwd(), "Department.csv")) == True : # Yes you wrote that part on your own too !
     print("The file has been generated !")  
 else :
     print("There is an error in the code !")
