78a28b8759c62dec19c3e648c13a88adfc38ab61
Anshu Sarkar <anshusarkar11@gmail.com>
2024-03-03 09:51:04 +0530

    echo

--- File_Handaling/File_Handaling.py
+++ File_Handaling/File_Handaling.py
@@ -1,4 +1,4 @@
-f = open("a.txt",'r') #To open a file a built in method of python get's used which fillows the 
+f = open("b.txt",'r') #To open a file a built in method of python get's used which fillows the 
                       # Following Syntax open("file name ",'operational command to be executed ')
                       # operational commands are = 'r', 'w', 'a'
                       # 'r'  for reading 
@@ -8,9 +8,9 @@ context = f.read()
 print(context)                     
 f.close()
 
-f=open("a.txt",'w')
+f=open("b.txt",'w')
 f.write("Hello _ File managemant\n")
 f.close()
 
-f=open("a.txt",'a')
+f=open("b.txt",'a')
 f.write("Hello _ This sentence is added using appemding")
--- File_Handaling/File_Reading.py
+++ File_Handaling/File_Reading.py
@@ -1,5 +1,6 @@
-f = open("a.txt",'r') #read() method reads entire line while the function readline() reads only one line 
+f = open("b.txt",'r') #read() method reads entire line while the function readline() reads only one line 
 context = f.read()
+var = f.readline() # reads only one line
 #print(context)
 contex_line_10 = []
 for contex_line_10 in range (10) :
