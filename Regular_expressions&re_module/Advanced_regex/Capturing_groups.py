52dc43412ab6917e17650f7468b66680dfc1ca45
anshusarkar <anshusarkar11@gmail.com>
2024-03-07 20:09:37 +0530

    echo

--- Regular_expressions&re_module/Advanced_regex/Capturing_groups.py
+++ Regular_expressions&re_module/Advanced_regex/Capturing_groups.py
@@ -22,3 +22,27 @@ print(result[2]) # This will return the second part of the string
 # Now using indexing the strings can be reversed 
 
 print("{} {}".format(result[2], result[1]))
+
+# checking if a string conatains exactaly five letter word in the sentence 
+
+print(re.search(r"[a-zA-Z]{5}", "a ghost is scary")) # In this case this case ghost is the five letter word
+
+# In the above even though the string conatained two words made of five latters it only returned the first match which is ghost
+# to fix that a method of the re class called .findall can the used
+
+print(re.findall(r"[a-zA-Z]{5}","a ghost is scary AF"))
+
+# But findall method might just return a string that has more than five letters 
+
+# To fix that \b can be used to in the raw string
+
+print(re.findall(r"\b[a-zA-Z]{5}\b", "a ghost is scary AF though you shouldn't get scared if you encounter one")) # this won't return thoug, should, scare, encou
+
+# the part under {} in the raw string part can be altered with ranges like if I want to gather strings with 5 charcters and 6 chracters in them then both the values can be passed to check the strings
+
+print(re.findall(r"\w{5,6}", "a GHOST IS SCARY AF so though you shouldn't get scared if you encounter one"))
+
+# searching for an word that starts with s\ but ends with upto 20 alpha numeric characters
+
+print(re.search(r"s\w{,20}", "I really like strawberries"))
+
