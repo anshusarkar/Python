572062086c8fbb96694afc218ba6e8ab60d8720a
anshusarkar <anshusarkar11@gmail.com>
2024-03-18 20:16:51 +0530

    echo

--- sys/sys.py
+++ sys/sys.py
@@ -19,3 +19,13 @@ print(sys.argv)
 # and for a faulty execution the system keeps a rercord in the form of 1
 
 # That is called exit ststus 
+
+# Dont return 1 that dosen't states a sucessfull execution for the system
+
+# Use 
+a=10
+b=12
+
+if a<b:
+    sys.exit(0) # To stste system a sucessfull execution 
+sys.exit(1) # To state an execution with error, that propably applies to all the progrsming languages meaning returning 0 doesn't state a sucessfull execution as system won't be aware of it ....
