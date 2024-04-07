6c61396dbf21b47bfc2c47ae8aaf11ce4ec82b4f
anshusarkar <anshusarkar11@gmail.com>
2024-04-07 18:57:12 +0530

    echo

--- Walrus.py
+++ Walrus.py
@@ -1,4 +1,4 @@
-val  : int
+val  : int # That's called type hinting in python 
 def fun ():
     return 2 + 3
 print(val :=fun()) # Walrus operator is the way of assinging values of a function to a variable
