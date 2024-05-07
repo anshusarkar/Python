e667e25336c727cbd56786d3b62d00dc3d35615b
anshusarkar <anshusarkar11@gmail.com>
2024-05-07 21:12:47 +0530

    echo

---
+++ Module,Packages&Libraries/main.py
@@ -0,0 +1,5 @@
+from module import add_two_numbers, subtract
+
+print(add_two_numbers(100, 200), subtract(999, 99))
+
+
---
+++ Module,Packages&Libraries/module.py
@@ -0,0 +1,10 @@
+# A python moudle is a pythno script that consists of many methods
+# that are users defined and can be reused 
+# by importing in another python script
+# to do so use from 'python_file_name_without_.py' import 'method name'
+
+def add_two_numbers(a:int, b:int)->int:
+    return a + b
+
+def subtract (a:int, b:int)->int:
+    return a - b
