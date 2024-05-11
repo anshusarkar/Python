5d0578c0e21a9f4a5fb9fd76806f7c2920bc86e5
anshusarkar <anshusarkar11@gmail.com>
2024-05-12 00:48:10 +0530

    echo

--- Flusk/Zero_Flask.py
+++ Flusk/Zero_Flask.py
@@ -6,4 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return "<b> My first application in action ! </>"
 
+
 app.run()
+
+# It's better to add a if__name__ == '__main__' over the app.run() statement as 
+# it would not let the script run if the script is imported as a 
+# module.... but why would you want to import it ?
+
