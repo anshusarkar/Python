f94c30f31a8b168a7459a9d4f01e855d0249c800
Anshu Sarkar <anshusarkar11@gmail.com>
2024-09-05 14:25:03 +0000

    echo

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,8 @@
-person = { "name" : "Anshu" ,
-          "title": "Sarkar" ,
-          "Birth_Date":19.01 }
+person = {
+    "name" : "Anshu" ,
+    "title": "Sarkar" ,
+    "Birth_Date":19.01 
+          }
 
 print(person["name"])
 
