21fc73e3b2f65baab3953090d9d11b40b59b294b
Anshu Sarkar <anshusarkar11@gmail.com>
2024-12-25 17:06:11 +0000

    echo

--- Tensorflow_module/tokenizer.py
+++ Tensorflow_module/tokenizer.py
@@ -4,12 +4,23 @@ from tensorflow import keras
 
 from tensorflow.keras.preprocessing.text import Tokenizer
 
-sentences = ['I love my dog', 'I love my cat']
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-tokenizer = Tokenizer(num_words = 100)
+
+sentences = ['I love my dog', 'I love my cat', 'You love my dog', 'I love my cat!', 'Do you think my dog is amazing?'] # The tokenizer won't produce a new tokken for the cat!
+
+tokenizer = Tokenizer(num_words = 100, oov_token='<OOV>') # The oov tokken is a word that represents a out of vocabulary token meaning it's a marker for tokkens that are out of context of tokkens
 
 tokenizer.fit_on_texts(sentences)
 
 word_index = tokenizer.word_index
 
+sequence = tokenizer.texts_to_sequences(sentences)
+
+padded = pad_sequences(sequence)
+
 print(word_index)
+
+print(sequence)
+
+print(padded)
