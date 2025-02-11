d47883278e95926ab96b8b713bf08ab1f7715720
anshu <anshusarkar11@gmail.com>
2025-02-11 13:08:03 +0000

    echo

---
+++ Django/hello/Neo/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+context = {
+        'varriable' : "this is sent",
+        'var2' : 'lalalal'
+}
+
+def index(request):
+    return render(request, 'index.html', context=context)
+def about(request):
+    # return HttpResponse("This is about page !")
+    return render(request, 'index.html', context=context)
+def services(request):
+    # return HttpResponse("This is services page !")
+    return render(request, 'index.html', context=context)
+def contact(request):
+    # return HttpResponse("This is contact page !")
+    return render(request, 'index.html', context=context)
---
+++ Django/hello/hello/urls.py
@@ -0,0 +1,18 @@
+
+from django.contrib import admin
+from django.urls import path
+from Neo import views
+
+# To make changes in the admin text in the admin panel uncommen the below text and make chnages in the strings
+
+# admin.site.site_header = "XYZ"
+# admin.site.site_title = "XYZ admin portal"
+# admin.site.index_title = "Welcome to the XYZ research portal"
+
+urlpatterns = [
+    path('', views.index, name = 'home'),
+    path('about/', views.about, name = 'about'),
+    path('services/', views.services, name = 'services'),
+    path('contact/', views.contact, name = 'contact'),
+     path('admin/', admin.site.urls)
+]
