5f5162bc8be72c7494647dc53fca1436b89c2986
anshu <anshusarkar11@gmail.com>
2025-02-12 07:54:27 +0000

    echo

--- Django/hello/Neo/views.py
+++ Django/hello/Neo/views.py
@@ -11,10 +11,10 @@ def index(request):
     return render(request, 'index.html', context=context)
 def about(request):
     # return HttpResponse("This is about page !")
-    return render(request, 'index.html', context=context)
+    return render(request, 'about.html', context=context)
 def services(request):
     # return HttpResponse("This is services page !")
-    return render(request, 'index.html', context=context)
+    return render(request, 'services.html', context=context)
 def contact(request):
     # return HttpResponse("This is contact page !")
-    return render(request, 'index.html', context=context)
+    return render(request, 'contact.html', context=context)
