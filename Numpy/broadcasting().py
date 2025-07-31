93d3ec0491013b1df6a0300754c410d8ca3bd5bb
Anshu Sarkar <81322177+anshusarkar@users.noreply.github.com>
2025-07-31 18:14:25 +0530

    Created broadcasting().py

---
+++ Numpy/broadcasting().py
@@ -0,0 +1,10 @@
+import numpy as np
+
+array1 = np.array([[1,2,3,4]]) #it's a 2d array with 1 row
+
+array2 = np.array([1],[2],[3],[4]) #it's a 2d array with 1 coloum 
+
+print(array1.shape)
+print(array2.shape)
+
+print(array1 * array2) #it's called array broadcasting 
