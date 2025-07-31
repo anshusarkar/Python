9613c3110476a038092fcfd52f200ad0c9afcee3
Anshu Sarkar <81322177+anshusarkar@users.noreply.github.com>
2025-07-31 18:15:08 +0530

    Update broadcasting().py

--- Numpy/broadcasting().py
+++ Numpy/broadcasting().py
@@ -7,4 +7,4 @@ array2 = np.array([1],[2],[3],[4]) #it's a 2d array with 1 coloum
 print(array1.shape)
 print(array2.shape)
 
-print(array1 * array2) #it's called array broadcasting 
+print(array1 * array2) #it's called array broadcasting, basically 2d array multiplication 
